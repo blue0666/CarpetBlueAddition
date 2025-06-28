4aa2c56ffdce934d032f27c3b5d23fa13b6a8ac1
blue0666 <blue6666@163.com>
2025-06-28 16:24:46 +0800

    盔甲架物品分身修改

--- src/main/java/com/blue0666/carpetblueaddition/mixins/rule/soundsuppressionintroduce/ArmorStandEntityMixin.java
+++ src/main/java/com/blue0666/carpetblueaddition/mixins/rule/soundsuppressionintroduce/ArmorStandEntityMixin.java
@@ -6,6 +6,8 @@ import com.blue0666.carpetblueaddition.settings.CarpetBlueAdditionSettings;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +19,10 @@ public class ArmorStandEntityMixin {
     @Inject(method = "equipStack", at = @At(value="RETURN"))
     public void emitSoundEvent(EquipmentSlot slot, ItemStack stack, CallbackInfo ci){
         if (CarpetBlueAdditionSettings.soundSuppressionIntroduce){
-            SoundEventManager.handleEntitySound((Entity)(Object)this,SoundEvent.ARMOR_EQUIP);
+            if (!stack.isEmpty()){
+                Item item = stack.getItem();
+                SoundEventManager.handleEntitySound((Entity)(Object)this, (item instanceof ArmorItem ? SoundEvent.ARMOR_EQUIP:SoundEvent.ARMOR_OTHER));
+            }
         }
     }
 }
